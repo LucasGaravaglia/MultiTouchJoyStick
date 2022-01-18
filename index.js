3a0d4ee6a1598967c4c091faf387e57a22c62f84
Lucas <lucasgrafimar@gmail.com>
2022-01-18 14:31:23 -0300

    Created multitouchJoystick

---
+++ index.js
@@ -0,0 +1,3 @@
+import MultitouchJoystick from "./src/index.js";
+
+export default MultitouchJoystick;
---
+++ src/index.js
@@ -0,0 +1,88 @@
+import React, { memo } from "react";
+import { View } from "react-native";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+
+import { PanGestureHandler } from "react-native-gesture-handler";
+
+function MultiTouchJoyStick({
+  backgroundColor = "#f5f5f5",
+  ballColor = "rgba(0, 0, 256, 0.5)",
+  ballRadius = 50,
+  height = 300,
+  width = 200,
+  refX,
+  refY,
+}) {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const panGestureEvent = useAnimatedGestureHandler({
+    onStart: () => {},
+    onActive: (event) => {
+      if (event.translationX < width / 2 && event.translationX > -width / 2)
+        translateX.value = event.translationX;
+      if (event.translationY < height / 2 && event.translationY > -height / 2)
+        translateY.value = event.translationY;
+      if (refY)
+        refY.current = Number(
+          (event.translationY / (height / 2 - ballRadius)).toFixed(2)
+        );
+      if (refX)
+        refX.current = Number(
+          (event.translationX / (width / 2 - ballRadius)).toFixed(2)
+        );
+    },
+    onEnd: () => {
+      if (refX) refX.current = 0;
+      if (refY) refY.current = 0;
+      translateX.value = withSpring(0);
+      translateY.value = withSpring(0);
+    },
+  });
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
+    };
+  });
+
+  return (
+    <View
+      style={{
+        width: width,
+        height: height,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: backgroundColor,
+      }}
+    >
+      <PanGestureHandler onGestureEvent={panGestureEvent}>
+        <Animated.View
+          style={[
+            {
+              width: ballRadius * 2,
+              height: ballRadius * 2,
+              backgroundColor: ballColor,
+              borderRadius: 180,
+            },
+            rStyle,
+          ]}
+        />
+      </PanGestureHandler>
+    </View>
+  );
+}
+
+export default memo(MultiTouchJoyStick);
